8029fc677f745108843cd2ac3dfa80b95670e170
August Sturm <auguststurm@users.noreply.github.com>
2020-09-03 19:33:41 -0700

    Updates

---
+++ precache-manifest.a64158710db1613fcbb0445bcfa92edb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a7481c9073560e51654b242caff2406",
+    "url": "/Porta/index.html"
+  },
+  {
+    "revision": "09613792fa3416e8cf13",
+    "url": "/Porta/static/css/main.9c5fce25.chunk.css"
+  },
+  {
+    "revision": "3b60a875c4b1269c98a5",
+    "url": "/Porta/static/js/2.47322455.chunk.js"
+  },
+  {
+    "revision": "9d94a441195015333c19654a45840997",
+    "url": "/Porta/static/js/2.47322455.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09613792fa3416e8cf13",
+    "url": "/Porta/static/js/main.13bdee06.chunk.js"
+  },
+  {
+    "revision": "26f3204ed8945c26f563",
+    "url": "/Porta/static/js/runtime-main.027e2b03.js"
+  }
+]);
