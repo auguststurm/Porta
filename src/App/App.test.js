bac50d57954baeae932ee643cfe98da1991a9541
August Sturm <auguststurm@users.noreply.github.com>
2020-09-03 18:45:05 -0700

    redundant for now

--- src/App/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-// 
-test('renders header branding', () => {
-  const { getByText } = render(<App />);
-  const brandText = getByText(/PORTA/i);
-  expect(brandText).toBeInTheDocument();
-});
