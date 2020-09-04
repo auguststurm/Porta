be2385be0b6f5ef5cdba63dbe6fe12a76859c0af
August Sturm <auguststurm@users.noreply.github.com>
2020-09-03 18:27:20 -0700

    better test description

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 // 
-test('renders learn react link', () => {
+test('renders header branding', () => {
   const { getByText } = render(<App />);
   const brandText = getByText(/PORTA/i);
   expect(brandText).toBeInTheDocument();
